02e5356976b335de4fab6986d44b6b5cc31f8fbe
Saheba-T <saheba.tegally@hotmail.com>
2022-06-18 00:42:54 -0700

    "Processed the data"

---
+++ 2022/week_24/US_drought.R
@@ -0,0 +1,35 @@
+
+
+# Load required libraries
+
+library(tidytuesdayR)
+library(tidyverse)
+library(lubridate)
+
+
+# Load the data
+
+tuesdata <- tidytuesdayR::tt_load('2022-06-14')
+drought <- tuesdata$drought
+
+
+# Clean the data 
+
+drought_clean <- drought %>% 
+  # Extract the part containing the date from the string 
+  dplyr::mutate(new_date = str_extract(DATE,"\\d+")) %>%
+  # Convert the date from string to a date object
+  dplyr::mutate(Date = lubridate::ymd(new_date)) %>%
+  # Filter only dates from the year 2001 to 2022
+  dplyr::filter(Date >= "2001-01-01")
+
+
+# Check for missing values
+
+sum(is.na(drought_clean))
+
+
+
+
+
+
