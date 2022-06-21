13820cc727b6948b3c04bba415416b92b453ca6b
Saheba-T <saheba.tegally@hotmail.com>
2022-06-20 21:04:13 -0700

    Finished and save the plot

--- 2022/week_24/US_drought.R
+++ 2022/week_24/US_drought.R
@@ -17,17 +17,27 @@ drought <- tuesdata$drought
 
 drought_clean <- drought %>% 
   # Extract the part containing the date from the string 
-  dplyr::mutate(new_date = str_extract(DATE,"\\d+")) %>%
+  dplyr::mutate(Date = str_extract(DATE,"\\d+")) %>%
   # Convert the date from string to a date object
-  dplyr::mutate(Date = lubridate::ymd(new_date)) %>%
+  dplyr::mutate(Date = lubridate::ymd(Date)) %>%
   # Filter only dates from the year 2001 to 2022
-  dplyr::filter(Date >= "2001-01-01")
+  dplyr::filter(Date >= "2001-01-01") %>%
+  # Capitalize the names of U.S. states
+  dplyr::mutate(State = str_to_title(state, locale = "en")) %>%
+  # Drop the original DATE and state column
+  dplyr::select(-c(DATE, state)) 
+
 
 
 # Check for missing values
 
 sum(is.na(drought_clean))
 
+
+# Perform EDA
+
+# Get overall summary of the data
+summary(drought_clean)
 
 
 
